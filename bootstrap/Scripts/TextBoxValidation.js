f49fb3e39231635bfd41efde43eaa72b49247337
aimi <95966270+oyaoyaimi@users.noreply.github.com>
2021-12-18 15:09:53 +0530

    first commit

---
+++ bootstrap/Scripts/TextBoxValidation.js
@@ -0,0 +1,60 @@
+﻿
+      function isNumberKey(evt)
+      {
+          var charCode = (evt.which) ? evt.which : event.keyCode
+          if (charCode > 31 && (charCode < 48 || charCode > 57))
+              return false;
+
+          return true;
+      }
+
+
+      var alpha = "[ A-Za-z]";
+      var numeric = "[0-9]";
+      var alphanumeric = "[ A-Za-z0-9.,/]";
+      var AlphName = "[ A-Za-z.]";
+
+      function onKeyValidate(e, charVal) {
+          var keynum;
+          var keyChars = /[\x00\x08]/;
+          var validChars = new RegExp(charVal);
+          if (window.event) {
+              keynum = e.keyCode;
+          }
+          else if (e.which) {
+              keynum = e.which;
+          }
+          var keychar = String.fromCharCode(keynum);
+          if (!validChars.test(keychar) && !keyChars.test(keychar)) {
+              return false
+          } else {
+              return keychar;
+          }
+      }
+
+      function ValidateEmail(mail) {
+          if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)) {
+              return (true)
+          }
+          toastr.warning('You have entered an invalid email address!');
+          return (false)
+      }
+      function displayToastr() {
+          toastr.options = {
+              "closeButton": true,
+              "debug": false,
+              "newestOnTop": false,
+              "progressBar": true,
+              "positionClass": "toast-bottom-right",
+              "preventDuplicates": false,
+              "onclick": null,
+              "showDuration": "300",
+              "hideDuration": "1000",
+              "timeOut": "5000",
+              "extendedTimeOut": "1000",
+              "showEasing": "swing",
+              "hideEasing": "linear",
+              "showMethod": "fadeIn",
+              "hideMethod": "fadeOut"
+          }
+      }
---
+++ bootstrap/Scripts/myScripts.js
@@ -0,0 +1,39 @@
+﻿function alertMe(e, f) {
+
+    toastr.options.positionClass = 'toast-top-center-ok'
+    toastr.options.timeOut = '3000'
+    toastr.options = {
+        "closeButton": true,
+        "debug": true,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": true,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    switch (f) {
+
+        case "Success":
+            toastr.success(e);
+            break;
+        case "Error":
+            toastr.error(e);
+            break;
+        case "Info":
+            toastr.info(e);
+            break;
+        case "Warning":
+            toastr.warning(e);
+            break;
+    }
+
+};
+
